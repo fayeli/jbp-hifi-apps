b8d1989daafb6d5ec0494783512419f77a9c7fa8
James B. Pollack <jamesbradenpollack@gmail.com>
2016-06-20 11:31:58 -0700

    working vive yo

--- feelspeak/recieve.js
+++ feelspeak/recieve.js
@@ -1,23 +1,26 @@
 var messagesReceivedCount = 0;
 
-var loudnessFactor = 0.05;
-
 function handleMessages(channel, message, sender) {
-    print('GOT MESSAGE')
-        // if (sender === MyAvatar.sessionUUID) {
-        //     return
-        // }
+        if (sender === MyAvatar.sessionUUID) {
+            return
+        }
 
     if (channel === 'feelspeak') {
         messagesReceivedCount++;
-        print('sendIndex/receiveCount::' + message + "/" + messagesReceivedCount);
+        // print('sendIndex/receiveCount::' + message + "/" + messagesReceivedCount);
+        vibrateControllers(JSON.parse(message).loudness)
     }
 }
 
 function vibrateControllers(loudness) {
-    var strength = scale(loudness, 0, 100, 0, 1)
+    var strength = scale(loudness, 0, 2500, 0, 1);
         //strength, duration, hand
-    Controller.triggerHapticPulse(strength, 1, 2)
+        print('strength is: ' + strength)
+        if(strength<0.1){
+            return
+        }
+    var vibrated = Controller.triggerHapticPulse(strength, 8, 2);
+    print('vibrated?' + vibrated)
 }
 
 function scale(value, min1, max1, min2, max2) {
