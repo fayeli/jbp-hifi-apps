32a31654caef93d023f23ac2bc30c05f31a939ef
James B. Pollack <jamesbradenpollack@gmail.com>
2016-08-30 18:10:15 -0700

    updates

--- gameTable/table.js
+++ gameTable/table.js
@@ -1,7 +1,7 @@
 (function() {
-    var GAMES_LIST_ENDPOINT = '';
+    var GAMES_LIST_ENDPOINT = 'https://api.myjson.com/bins/2p22c';
     var _this;
-    var INITIAL_DELAY = 1000;
+    var INITIAL_DELAY = 3000;
 
     function GameTable() {
         _this = this;
@@ -9,20 +9,28 @@
 
     GameTable.prototype = {
         matCorner: null,
+        currentGameIndex: 0,
         preload: function(entityID) {
             _this.entityID = entityID;
             Script.setTimeout(function() {
                 _this.setCurrentGamesList();
-                _this.setInitialGameIfNone();
             }, INITIAL_DELAY);
         },
         setInitialGameIfNone: function() {
             var userData = _this.getCurrentUserData();
-            if (userData.hasOwnProperty('currentGame') !== true) {
-                _this.setCurrentGame(_this.gamesList[0]);
-                _this.setCurrentUserData(userData);
-            };
-            _this.resetGame();
+        
+            if (userData.hasOwnProperty('gameTable') !== true || userData.gameTable.hasOwnProperty('currentGame')!==true) {
+                print('userdata has no gametable or no currentgame')
+                _this.setCurrentGame();
+                _this.resetGame();
+                print('i set the game and reset the game')
+            }
+            else{
+                print('already has game')
+            }
+
+
+           
         },
         resetGame: function() {
             print('RESET GAME on gameTable')
@@ -32,7 +40,7 @@
         nextGame: function() {
             print('NEXT GAME on gameTable')
             _this.currentGameIndex++;
-            if (_this.currentGameIndex > _this.gamesList.length) {
+            if (_this.currentGameIndex >= _this.gamesList.length) {
                 _this.currentGameIndex = 0;
             }
             _this.cleanupGameEntities();
@@ -43,31 +51,37 @@
 
         },
         setCurrentGamesList: function() {
-            var userData = _this.getCurrentUserData();
-            userData.gamesList = getGamesList();
-            _this.gamesList = userData.gamesList;
-            _this.setCurrentUserData(userData);
+            var gamesList = getGamesList();
+            _this.gamesList = gamesList;
+            print('set gameslist to: ' + JSON.stringify(gamesList))
+            _this.setInitialGameIfNone();
         },
         setCurrentGame: function() {
-            var userData = _this.getCurrentUserData();
-            userData.currentGame = _this.gamesList[_this.currentGameIndex];
-            _this.currentGame = serData.currentGame;
-            _this.setCurrentUserData(userData);
+            print('games list in set current game' + _this.gamesList);
+            _this.currentGame = _this.gamesList[_this.currentGameIndex].gameName;
+
+            _this.setCurrentUserData({
+                currentGame: _this.currentGame
+            });
         },
         setCurrentUserData: function(data) {
-            var userData = getCurrentUserData();
-            userData.gameTableData = data;
-            Entities.editEntity(_this.entityID, {
-                userData: userData
+            var userData = _this.getCurrentUserData();
+            userData['gameTableData'] = data;
+            var success = Entities.editEntity(_this.entityID, {
+                userData: JSON.stringify(userData),
             });
         },
         getCurrentUserData: function() {
             var props = Entities.getEntityProperties(_this.entityID);
-            var json = null;
+            var hasUserData = props.hasOwnProperty('userData');
+            print('has user data??'+hasUserData)
+            print('userData is:: '+props.userData)
+            var json = {};
             try {
                 json = JSON.parse(props.userData);
             } catch (e) {
-                return;
+                print('user data is not json' + props.userData)
+                return json;
             }
             return json;
         },
@@ -104,6 +118,7 @@
         request.send();
 
         var response = JSON.parse(request.responseText);
+        print('got gamesList' + response.length);
         return response;
     };
 
